e25c2289da9329c4cb8e989662af720ae6a13d1a
Richard Paredes <chard.paredes@gmail.com>
2018-10-17 22:38:54 +0200

    Creating Header.jsx file for the component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div class="bg-image" />
+      <h1>THREAD Project</h1>
+    </header>
+  );
+};
+
+export default Header;
