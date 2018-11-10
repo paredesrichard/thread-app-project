c33aa372796125731f78bf5b9daa78669b66df4d
Jack <nrickygrant@gmail.com>
2018-11-10 19:46:52 +0100

    added delete function

--- src/client/components/MentorsView/Card.js
+++ src/client/components/MentorsView/Card.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Card.css';
 import {Link} from "react-router-dom";
 
+/*
 const Card = props => {
+  
   return (
     <div className="mentors-card">
       <img
@@ -10,12 +12,6 @@ const Card = props => {
         className="thumb-nail-img"
         alt="thumbnail"/>
       <p>
-      {/*
-        <Link
-          to={`/mentors/edit/${props.data.id}`}
-          className="btn btn-primary mb-2"
-          target="_blank">Edit</Link>
-  */}
         <br/>
         <span className="card-label">First</span>
         :{' '} {props.data.first_name}
@@ -38,12 +34,73 @@ const Card = props => {
           target="_blank">Edit</Link>
           </div>
           <Link to="#"  className="btn btn-primary mb-2">
-          Deleted
+          Delete
         </Link>
       </div>
       
     </div>
   );
 };
+*/
+
+class Card extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  deleteRecord(id) {
+    console.log("deleting record id: ", id);
+
+    fetch(`/api/internships/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.text())
+      .then(response => {
+        console.log('Success:', response)
+        alert("Record has been deleted");
+      })
+      .catch(error => console.error('Error:', error));
+  }
+  
+  render(){
+    return (
+      <div className="mentors-card">
+        <img
+          src={this.props.data.profile_picture}
+          className="thumb-nail-img"
+          alt="thumbnail"/>
+        <p>
+          <br/>
+          <span className="card-label">First</span>
+          :{' '} {this.props.data.first_name}
+          <br/>
+          <span className="card-label">Last Name</span>
+          :{' '} {this.props.data.last_name}
+          <br/>
+          <span className="card-label">Email address</span>
+          :{' '} {this.props.data.email}
+          <br/>
+          <span className="card-label">Description</span>
+          :{' '} {this.props.data.mentor_description}
+        </p>
+        
+        <div className="form-row">
+        <div className="col-auto">
+          <Link
+            to={`/mentors/edit/${this.props.data.id}`}
+            className="btn btn-primary mb-2"
+            target="_blank">Edit</Link>
+            </div>
+            <div className="col-auto">
+              <button class="btn btn-primary btn-sm" onClick={() => { if (window.confirm('Are you sure you wish to delete this record?')) this.deleteRecord(this.props.data.id) }}>Delete</button>
+            </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
 
 export default Card;
