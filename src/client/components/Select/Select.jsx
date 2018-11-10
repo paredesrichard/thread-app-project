a52264f45457641bf92125c57f625a82a045c82d
Jack <nrickygrant@gmail.com>
2018-11-10 15:45:16 +0100

    corrected the eventspage

--- src/client/components/Select/Select.jsx
+++ src/client/components/Select/Select.jsx
@@ -4,7 +4,7 @@ import "./Select.css";
 const Select = () => {
   return (
     <div className="select-container">
-      <select id="categories" value={this.state.data.categories} name="categories" required>
+      <select id="categories"  name="categories" required>
         <option value="">None</option>
         <option value="Internships">Internships</option>
         <option value="Events">Events</option>
