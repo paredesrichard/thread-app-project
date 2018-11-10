7771ce3db46a10306642564b158064f59d6eabd0
Richard Paredes <chard.paredes@gmail.com>
2018-11-10 21:37:31 +0100

    Added the login/logout functionality to the app

--- src/client/App.js
+++ src/client/App.js
@@ -26,35 +26,38 @@ import EditInternships from './components/InternshipsForm/EditInternships';
 import MentorSearch from './components/MentorsView/MentorSearch';
 import NetworkingSearch from './components/NetworkingView/NetworkingSearch';
 import Login from './components/Login';
-// import Logout from "./components/logout";
-// import LoginContext, { loadContextValue } from './contexts/login';
+import Logout from "./components/Logout/Logout";
+import LoginContext, { loadContextValue } from './contexts/login';
 
 // console.log(AddMentor);
 class App extends Component {
   render() {
-    // const contextValue = loadContextValue();
+    const contextValue = loadContextValue();
     return (
       <div className="container-fluid m-0 p-0">
-        <Header />
-        <Navmenu />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/internships" component={InternshipsView} />
-          <Route exact path="/admin/internships/add" component={InternshipsForm} />
-          <Route exact path="/admin/internships/edit/:id" component={EditInternships} />
-          <Route exact path="/events" component={EventsView} />
-          <Route exact path="/mentors" component={MentorsView} />
-          <Route exact path="/networking" component={NetworkingView} />
-          <Route exact path="/about" component={AboutUs} />
-          <Route exact path="/mentors/add" component={MentorForms.AddMentor} />
-          <Route exact path="/mentors/edit/:id" component={MentorForms.EditMentor} />
-          <Route exact path="/Mentors/search" component={MentorSearch} />
-          <Route exact path="/networking/add" component={NetworkingForms.AddNetworking} />
-          <Route exact path="/networking/edit/:id" component={NetworkingForms.EditNetworking} />
-          <Route exact path="/Networking/search" component={NetworkingSearch} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        <Footer />
+        <LoginContext.Provider value={contextValue}>
+          <Header />
+          <Navmenu />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/internships" component={InternshipsView} />
+            <Route exact path="/admin/internships/add" component={InternshipsForm} />
+            <Route exact path="/admin/internships/edit/:id" component={EditInternships} />
+            <Route exact path="/events" component={EventsView} />
+            <Route exact path="/mentors" component={MentorsView} />
+            <Route exact path="/networking" component={NetworkingView} />
+            <Route exact path="/about" component={AboutUs} />
+            <Route exact path="/mentors/add" component={MentorForms.AddMentor} />
+            <Route exact path="/mentors/edit/:id" component={MentorForms.EditMentor} />
+            <Route exact path="/Mentors/search" component={MentorSearch} />
+            <Route exact path="/networking/add" component={NetworkingForms.AddNetworking} />
+            <Route exact path="/networking/edit/:id" component={NetworkingForms.EditNetworking} />
+            <Route exact path="/Networking/search" component={NetworkingSearch} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/logout" component={Logout} />
+          </Switch>
+          <Footer />
+        </LoginContext.Provider>
       </div>
     );
   }
