3bcfefc7ba5a0703acdd1af4ef4e1ee19ecde112
Richard Paredes <chard.paredes@gmail.com>
2018-11-10 17:38:59 +0100

    Modified the Login component

--- src/client/components/Login/component.js
+++ src/client/components/Login/component.js
@@ -1,76 +1,96 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 //import SearchForm from '../SearchForm/SearchForm';
+import './Login.css';
+import { NavLink } from 'react-router-dom';
 
-class Login extends Component{
-    state = {
-        user: {
-          email: "",
-          password: ""
-        }
-      };
+class Login extends Component {
+  state = {
+    user: {
+      email: '',
+      password: '',
+    },
+  };
 
-      onFieldChange = event => {
-        const { name, value } = event.target;
-    
-        this.setState({
-          user: {
-            ...this.state.user,
-            [name]: value
-          }
-        });
-      };
-      onSubmit = event => {
-        event.preventDefault();
-    
-        fetch("/api/auth/login", {
-          method: "POST",
-          body: JSON.stringify(this.state.user),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-          .then(res => {
-            return res.json();
-          })
-          .then(response => {
-            console.log("Success:", JSON.stringify(response));
-            localStorage.setItem('authToken', response.token);
-    
-            // TODO maybe add a message showing "Logged in Successfully";
-            // Or use react context, to show a logout button instead of login in the header
-    
-            this.props.history.push('/');
-          })
-          .catch(error => console.error("Error:", error));
-      };
-    
-      render() {
-        return (
-          <div className="container">
-          <h3>Welcome login page</h3>
-            <form onSubmit={this.onSubmit}>
-              <label>
-                E-mail
-                <input
-                  type="email"
-                  name="email"
-                  value={this.state.user.email}
-                  onChange={this.onFieldChange}
-                />
-              </label>
-              <label>
-                Password
-                <input
-                  type="password"
-                  name="password"
-                  value={this.state.user.password}
-                  onChange={this.onFieldChange}
-                />
-              </label>
-              <button type="submit">Submit</button>
-            </form>
+  onFieldChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      user: {
+        ...this.state.user,
+        [name]: value,
+      },
+    });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+
+    fetch('/api/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(this.state.user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(response => {
+        console.log('Success:', JSON.stringify(response));
+        localStorage.setItem('authToken', response.token);
+
+        // TODO maybe add a message showing "Logged in Successfully";
+        // Or use react context, to show a logout button instead of login in the header
+
+        this.props.history.push('/');
+      })
+      .catch(error => console.error('Error:', error));
+  };
+
+  render() {
+    return (
+      <div className="login-form">
+        <form onSubmit={this.onSubmit}>
+          <h2 className="text-center">Log in</h2>
+          <div className="form-group">
+            <input
+              name="email"
+              type="text"
+              className="form-control"
+              placeholder="Email address"
+              required="required"
+              onChange={this.onFieldChange}
+              value={this.state.user.email}
+            />
           </div>
-        );
-      }
-    }
+          <div className="form-group">
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              required="required"
+              onChange={this.onFieldChange}
+              value={this.state.user.password}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary btn-block">
+              Log in
+            </button>
+          </div>
+          <div className="clearfix">
+            <NavLink to="#" className="text-primary">
+              Forgot Password?
+            </NavLink>
+          </div>
+        </form>
+        <p className="text-center">
+          <NavLink to="#" className="text-primary">
+            Create an Account
+          </NavLink>
+        </p>
+      </div>
+    );
+  }
+}
 export default Login;
