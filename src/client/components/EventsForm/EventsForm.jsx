270e1c4b21a3ca8b89aa25feacf8c7272ceceeb0
Richard Paredes <chard.paredes@gmail.com>
2018-11-16 12:45:02 +0100

    Created initial EventsForm component

---
+++ src/client/components/EventsForm/EventsForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EventsForm extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Welcome to Events Admin Form</h4>
+      </div>
+    )
+  }
+}
