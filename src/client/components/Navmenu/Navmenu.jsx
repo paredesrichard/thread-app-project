37279c406c59b816a94853b70e09a48c7b01fea6
Richard Paredes <chard.paredes@gmail.com>
2018-11-21 22:43:02 +0100

    Modified the Navmenu to make it collapsible

--- src/client/components/Navmenu/Navmenu.jsx
+++ src/client/components/Navmenu/Navmenu.jsx
@@ -9,52 +9,78 @@ class Navmenu extends Component {
   render() {
     const contextType = LoginContext._currentValue;
     return (
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink exact to="/" className="nav-link">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/internships" className="nav-link">
-              Internships
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/events" className="nav-link">
-              Events
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/mentors" className="nav-link">
-              Mentors
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/networking" className="nav-link">
-              Networking
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/about" className="nav-link">
-              About Us
-            </NavLink>
-          </li>
-          {contextType.isLoggedIn ? (
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+        <a className="navbar-brand mr-5" href="/">
+          <img
+            src="/src/client/images/logo.svg"
+            width="50"
+            height="50"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          THREAD
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          className="collapse navbar-collapse mr-auto"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to="/logout" className="nav-link">
-                Logout
+              <NavLink exact to="/" className="nav-link">
+                Home
               </NavLink>
             </li>
-          ) : (
             <li className="nav-item">
-              <NavLink to="/login" className="nav-link">
-                Login
+              <NavLink to="/internships" className="nav-link">
+                Internships
               </NavLink>
             </li>
-          )}
-        </ul>
+            <li className="nav-item">
+              <NavLink to="/events" className="nav-link">
+                Events
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/mentors" className="nav-link">
+                Mentors
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/networking" className="nav-link">
+                Networking
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About Us
+              </NavLink>
+            </li>
+            {contextType.isLoggedIn ? (
+              <li className="nav-item">
+                <NavLink to="/logout" className="nav-link">
+                  Logout
+                </NavLink>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
       </nav>
     );
   }
