3b2b64daa0f9e2b1beda5fbb670f3adf44b8fb6b
Richard Paredes <chard.paredes@gmail.com>
2018-11-24 20:27:05 +0100

    Adjusted font size, modified the background image to use the local copy instead of pointing the link to a remote URL

--- src/client/components/Homepage/Homepage.jsx
+++ src/client/components/Homepage/Homepage.jsx
@@ -4,10 +4,10 @@ import './Homepage.css';
 
 const Homepage = () => (
   <div className="homepage-container font-weight-normal px-5">
-    <h1 className="welcome-page text-white">Welcome to the Thread Project</h1>
+    <h1 className="welcome-page text-white">Welcome to the Thread Online Platform Project</h1>
     <div className="container text-white">
-      <h4>THREAD</h4>
-      <h6>Textile Hub for Refugees' Empowerment, Employment and Entrepreneurship Advancement in Denmark - a new model</h6>
+      <h4 className="p-5">THREAD</h4>
+      <h5 className="p-4">Textile Hub for Refugees' Empowerment, Employment and Entrepreneurship Advancement in Denmark - a new model</h5>
       <p className="text-center">
         THREAD is a dynamic and innovative collaboration of diverse partners
         from business, design, education, research and refugee support agencies
