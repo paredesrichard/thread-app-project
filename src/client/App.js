2d17c13d2ddb8ddb5ab8b71aff248799e0a15e87
Richard Paredes <richardparedes@munindata.com>
2019-12-22 01:47:04 +0100

    removed unused codes on App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
@@ -5,29 +6,21 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Navmenu from './components/Navmenu/Navmenu';
-// import SearchForm from './components/SearchForm/SearchForm';
 import Footer from './components/Footer/Footer';
-// import ListView from './components/ListView/ListView';
-// import Calendar from './components/calendar/calendar';
 import Homepage from './components/Homepage/Homepage';
 import AboutUs from './components/AboutUs/AboutUs';
 import EventsView from './components/EventsView/EventsView';
 import InternshipsView from './components/InternshipsView/InternshipsView';
 import MentorsView from './components/MentorsView/MentorsView';
 import NetworkingView from './components/NetworkingView/NetworkingView';
-//import NetworkingDetailedView from "./components/NetworkingView/NetworkingDetailedView";
-
-import MentorForms from './components/add';
 
 import NetworkingForms from './components/networkingForm';
 
 import EventsForm from './components/EventsForm/EventsForm';
 import EditEvents from './components/EventsForm/EditEvents';
 import InternshipsForm from './components/InternshipsForm/InternshipsForm';
-import AddInternship from './components/InternshipsForm/AddInternship';
 import EditInternships from './components/InternshipsForm/EditInternships';
 import DeleteRecord from './components/DeleteRecord/DeleteRecord';
-import MentorSearch from './components/MentorsView/MentorSearch';
 import NetworkingSearch from './components/NetworkingView/NetworkingSearch';
 import Login from './components/Login';
 import Logout from "./components/Logout/Logout";
@@ -65,9 +58,6 @@ class App extends Component {
             <Route exact path="/admin/mentors/delete/:id" component={DeleteRecord} />
             <Route exact path="/networking" component={NetworkingView} />
             <Route exact path="/about" component={AboutUs} />
-            {/* <Route exact path="/mentors/add" component={MentorForms.AddMentor} /> 
-            <Route exact path="/mentors/edit/:id" component={MentorForms.EditMentor} /> 
-            <Route exact path="/Mentors/search" component={MentorSearch} /> */}
             <Route exact path="/networking/add" component={NetworkingForms.AddNetworking} />
             <Route exact path="/networking/edit/:id" component={NetworkingForms.EditNetworking} />
             <Route exact path="/admin/networking/delete/:id" component={DeleteRecord} />
