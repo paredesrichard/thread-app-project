a16680c83848ed7923a2156d6992ad088e0244c8
Richard Paredes <richardparedes@munindata.com>
2019-12-22 01:16:04 +0100

    Removed DB connection string from db.config

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql2';
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || 'mysql://hyf:password@127.0.0.1:3306/hyf_thread';
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
 const connection = mysql.createConnection(DATABASE_URL);
 
 export default connection;
